2cfb9aa4bd790f1f476a6772c5e8398050672dbd
Y31low <samuele.giallorenzo@gmail.com>
2024-12-18 17:15:40 +0100

    nada

--- src/main/java/org/uniupo/it/service/AssistanceService.java
+++ src/main/java/org/uniupo/it/service/AssistanceService.java
@@ -1,4 +1,24 @@
 package org.uniupo.it.service;
 
+import com.google.gson.Gson;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class AssistanceService {
+    final private String machineId;
+    final private MqttClient mqttClient;
+    final private String baseTopic;
+    final private Gson gson;
+
+    public AssistanceService(String machineId, MqttClient mqttClient) throws MqttException {
+        this.machineId = machineId;
+        this.mqttClient = mqttClient;
+        this.baseTopic = "macchina/" + machineId + "/assistance";
+        this.gson = new Gson();
+        this.mqttClient.subscribe(baseTopic + "/request", this::requestHandler);
+    }
+
+    private void requestHandler(String s, MqttMessage mqttMessage) {
+    }
 }
